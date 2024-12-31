fffb61f1545e5063d99bd0d46ffca3824f40e7f0
jlxnb <2747789919@qq.com>
2024-12-31 21:03:42 +0800

    enable bstats

--- src/main/java/org/eu/pcraft/powerfulfireworks/PowerfulFireworks.java
+++ src/main/java/org/eu/pcraft/powerfulfireworks/PowerfulFireworks.java
@@ -1,13 +1,15 @@
 package org.eu.pcraft.powerfulfireworks;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PowerfulFireworks extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        ////bStats////
+        int pluginId = 21763;
+        Metrics metrics = new Metrics(this, pluginId);
     }
 
     @Override
