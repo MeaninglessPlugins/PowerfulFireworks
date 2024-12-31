702efe143a8b01a46dfbd5086badac607ec11aca
jlxnb <2747789919@qq.com>
2024-12-31 20:22:14 +0800

    Initial commit

---
+++ src/main/java/org/eu/pcraft/powerfulfireworks/PowerfulFireworks.java
@@ -0,0 +1,17 @@
+package org.eu.pcraft.powerfulfireworks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PowerfulFireworks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
