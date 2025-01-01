555829cdec6fdc223603359a77929f58ed79484f
jlxnb <2747789919@qq.com>
2025-01-01 12:53:40 +0800

    optimize

--- src/main/java/org/eu/pcraft/powerfulfireworks/FireworksTimer.java
+++ src/main/java/org/eu/pcraft/powerfulfireworks/FireworksTimer.java
@@ -2,7 +2,10 @@ package org.eu.pcraft.powerfulfireworks;
 
 import org.bukkit.FireworkEffect;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.FireworkMeta;
+import org.eu.pcraft.powerfulfireworks.nms.common.NMSEntityDataPacket;
+import org.eu.pcraft.powerfulfireworks.nms.common.NMSEntityEventPacket;
 import org.eu.pcraft.powerfulfireworks.nms.common.NMSPlayer;
 import org.eu.pcraft.powerfulfireworks.nms.common.NMSProvider;
 import org.eu.pcraft.powerfulfireworks.utils.FireworkUtil;
@@ -16,15 +19,17 @@ public class FireworksTimer extends PepperRollTimer{
     }
     @Override
     protected void run(){
+        final PowerfulFireworks pl = PowerfulFireworks.getInstance();
+        final NMSProvider provider = pl.getNms();
+        NMSEntityDataPacket fakeFirework = provider.createFireworkEntityDataPacket(114514, fireworkUtil.getRandomFireworkItem());
+        NMSEntityEventPacket eventPacket=provider.createEntityEvent(114514, (byte) 17);
         for(Player player:plugin.getServer().getOnlinePlayers()){
-            final PowerfulFireworks pl = PowerfulFireworks.getInstance();
-            final NMSProvider provider = pl.getNms();
             final NMSPlayer nms = provider.getPlayer(player);
             PowerfulFireworks.getInstance().nextTick(() -> {
                 UUID uuid = UUID.randomUUID();
-                nms.sendFakeFirework(114514, uuid, fireworkUtil.getRandomLocation(player), provider.createFireworkEntityDataPacket(114514, fireworkUtil.getRandomFireworkItem()));
+                nms.sendFakeFirework(114514, uuid, fireworkUtil.getRandomLocation(player), fakeFirework);
                 pl.nextTick(() -> {
-                    nms.sendEntityEvent(provider.createEntityEvent(114514, (byte) 17)); // firework explosion
+                    nms.sendEntityEvent(eventPacket); // firework explosion
                     nms.sendRemoveEntity(114514);
                 });
             });
