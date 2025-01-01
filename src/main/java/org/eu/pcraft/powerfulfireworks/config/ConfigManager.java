56e510aef198a4a3faf38388253e85aefea7c97d
jlxnb <2747789919@qq.com>
2025-01-01 09:53:56 +0800

    update config system

--- src/main/java/org/eu/pcraft/powerfulfireworks/config/ConfigManager.java
+++ src/main/java/org/eu/pcraft/powerfulfireworks/config/ConfigManager.java
@@ -4,6 +4,7 @@ import org.eu.pcraft.powerfulfireworks.PowerfulFireworks;
 import org.spongepowered.configurate.CommentedConfigurationNode;
 import org.spongepowered.configurate.ConfigurateException;
 import org.spongepowered.configurate.ConfigurationOptions;
+import org.spongepowered.configurate.objectmapping.meta.Processor;
 import org.spongepowered.configurate.yaml.NodeStyle;
 import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
 
@@ -12,26 +13,27 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import static org.spongepowered.configurate.objectmapping.meta.Processor.comments;
+
 public class ConfigManager {
     CommentedConfigurationNode node;
 
     YamlConfigurationLoader loader;
     public PepperConfigModule configModule;
     public ConfigManager(Path src,PowerfulFireworks inst){
-        if (Files.notExists(src)){
-            inst.saveResource("config.yml",false);
-        }
         loader = YamlConfigurationLoader.builder()
                 .nodeStyle(NodeStyle.BLOCK)
                 .indent(2)
                 .path(src) // Set where we will load and save to
                 .build();
         node=loader.createNode();
+//        node.comment("PowerfulFireworks配置文件");
     }
     public void loadConfig(){
         try {
             node=loader.load();
             configModule=node.get(PepperConfigModule.class);
+            saveConfig();
         }catch (ConfigurateException e){
             throw new RuntimeException(e);
         }
@@ -39,6 +41,7 @@ public class ConfigManager {
     public void saveConfig(){
         try {
             node.set(configModule);
+//            Processor.comments();
             loader.save(node);
         }catch (ConfigurateException e){
             throw new RuntimeException(e);
--- src/main/java/org/eu/pcraft/powerfulfireworks/config/PepperConfigModule.java
+++ src/main/java/org/eu/pcraft/powerfulfireworks/config/PepperConfigModule.java
@@ -7,6 +7,8 @@ import org.spongepowered.configurate.objectmapping.meta.Comment;
 public class PepperConfigModule {
     @Comment("PowerfulFireworks配置文件") String a;
     @Comment("Written by:Pepper&H3xadecimal") String b;
+    @Comment("版本信息,请不要修改! ")
+    public String configVersion = "0.0.1";
     public boolean debug = false;
 
 }
