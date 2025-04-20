099b4d87da091edeab26a6ba1491cafe8233fe3c
Zyklone <h3xadecimal@afternode.cn>
2025-04-20 13:03:49 +0800

    feat: 1.21.5

--- nms/src/main/java/org/eu/pcraft/powerfulfireworks/nms/NMSSelector.java
+++ nms/src/main/java/org/eu/pcraft/powerfulfireworks/nms/NMSSelector.java
@@ -5,7 +5,7 @@ import org.eu.pcraft.powerfulfireworks.nms.common.NMSProvider;
 public class NMSSelector {
     public static NMSProvider getImplementation(String version) {
         return switch (version) {
-            case "1.21.4", "1.21.3", "1.21.2", "1.21.1", "1.21", "1.20.6", "1.20.5" ->
+            case "1.21.5", "1.21.4", "1.21.3", "1.21.2", "1.21.1", "1.21", "1.20.6", "1.20.5" ->
                     new org.eu.pcraft.powerfulfireworks.nms.v1_21_4.NMSProviderImpl();
             case "1.20.4" -> new org.eu.pcraft.powerfulfireworks.nms.v1_20_4.NMSProviderImpl();
             case "1.20.2" -> new org.eu.pcraft.powerfulfireworks.nms.v1_20_2.NMSProviderImpl();
