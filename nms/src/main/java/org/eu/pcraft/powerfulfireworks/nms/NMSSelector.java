0849856270e9a13b82adbd977c0ae36c1de6c9c4
jlxnb <97275132+jlxnb@users.noreply.github.com>
2026-01-01 01:52:11 +0800

    feat:修改版本判断逻辑&支持默认最新版本

--- nms/src/main/java/org/eu/pcraft/powerfulfireworks/nms/NMSSelector.java
+++ nms/src/main/java/org/eu/pcraft/powerfulfireworks/nms/NMSSelector.java
@@ -1,17 +1,50 @@
 package org.eu.pcraft.powerfulfireworks.nms;
 
 import org.eu.pcraft.powerfulfireworks.nms.common.NMSProvider;
+import org.eu.pcraft.powerfulfireworks.nms.v1_21_4.NMSProviderImpl;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import static java.util.Map.entry;
 
 public class NMSSelector {
+    static List<Map.Entry<String, Supplier<NMSProvider>>> versionList = List.of(
+            entry("1.20.5", NMSProviderImpl::new),
+            entry("1.20.4", org.eu.pcraft.powerfulfireworks.nms.v1_20_4.NMSProviderImpl::new),
+            entry("1.20.2", org.eu.pcraft.powerfulfireworks.nms.v1_20_2.NMSProviderImpl::new),
+            entry("1.19.4", org.eu.pcraft.powerfulfireworks.nms.v1_19_4.NMSProviderImpl::new),
+            entry("1.18.2", org.eu.pcraft.powerfulfireworks.nms.v1_18_2.NMSProviderImpl::new)
+    );
+    private static List<Integer> parseVersion(String version) {
+        List<Integer> parts = new ArrayList<>();
+        Scanner scanner = new Scanner(version);
+        try(scanner){
+            scanner.useDelimiter("\\.");
+            while (scanner.hasNextInt()) {
+                parts.add(scanner.nextInt());
+            }
+        }
+        return parts;
+    }
+
+    // 如果L1版本大于等于L2，返回true，否则返回false
+    private static boolean compareVersion(List<Integer> l1,List<Integer> l2){
+        int length = Math.min(l1.size(), l2.size());
+        for(int i=0;i<length;i++){
+            if(l1.get(i).equals(l2.get(i)))continue;
+            return l1.get(i)>l2.get(i);
+        }
+        return l1.size()>=l2.size();
+    }
+
     public static NMSProvider getImplementation(String version) {
-        return switch (version) {
-            case "1.21.5", "1.21.4", "1.21.3", "1.21.2", "1.21.1", "1.21", "1.20.6", "1.20.5" ->
-                    new org.eu.pcraft.powerfulfireworks.nms.v1_21_4.NMSProviderImpl();
-            case "1.20.4" -> new org.eu.pcraft.powerfulfireworks.nms.v1_20_4.NMSProviderImpl();
-            case "1.20.2" -> new org.eu.pcraft.powerfulfireworks.nms.v1_20_2.NMSProviderImpl();
-            case "1.19.4" -> new org.eu.pcraft.powerfulfireworks.nms.v1_19_4.NMSProviderImpl();
-            case "1.18.2" -> new org.eu.pcraft.powerfulfireworks.nms.v1_18_2.NMSProviderImpl();
-            default -> null;
-        };
+        for(Map.Entry<String, Supplier<NMSProvider>> entry:versionList){
+            if(compareVersion(parseVersion(version),parseVersion(entry.getKey()))){
+                return entry.getValue().get();
+            }
+        }
+        return null;
     }
 }
